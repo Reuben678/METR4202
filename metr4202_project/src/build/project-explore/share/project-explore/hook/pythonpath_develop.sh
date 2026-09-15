6733673542d07c484509a681082d936b90314a5c
sudo-mjc <mitchellcraw64@gmail.com>
2026-09-15 12:10:37 +1000

    updated project repo to guidelines

--- metr4202_project/src/build/project-explore/share/project-explore/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/mjc/METR4202/METR4202/metr4202_project/src/build/project-explore"
--- metr4202_project/src/build/project-search/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mjc/METR4202/METR4202/metr4202_project/src/install/project-search'
