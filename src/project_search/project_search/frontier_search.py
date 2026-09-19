4469baadcfbdce6f6b2149b355f6aa47ddc2947c
sudo-mjc <mitchellcraw64@gmail.com>
2026-09-19 23:33:56 +1000

    FrontierArray data structure for publishing frontier information to MST planner

--- src/project_search/project_search/frontier_search.py
+++ src/project_search/project_search/frontier_search.py
@@ -33,6 +33,7 @@ from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid
 from nav2_msgs.msg import BehaviorTreeLog
 from geometry_msgs.msg import PoseStamped
+from frontier_msgs.msg import FrontierArray
 
 import numpy as np
 from scipy import ndimage
@@ -40,75 +41,14 @@ from scipy import ndimage
 
 FREE, UNKNOWN, OCCUPIED = 0, -1, 100    # Defined macros for trinary cell values
 
-MIN_FRONTIER_SIZE = 3   # Min size of frontier allowable 
+MIN_FRONTIER_SIZE = 5   # Min size of frontier allowable 
+MIN_FRONTIER_DIST = 3
 
 MAP_FRAME = "map"
 ROBOT_FRAME = "base_link"
 
 BT_TRIGGER_NODE = "NavigateRecovery"
 BT_TRIGGER_STATUS = ("SUCCESS", "FAILURE")
-
-
-"""
-Find cells which form a frontier (FREE cells bordering UNKNOWN cells)
-"""
-def find_frontier_mask(grid):
-    free_mask = grid == FREE
-    unknown_mask = grid == UNKNOWN
-
-    # Copy contents of unknown_mask, with same data type and shape
-    neighbour_unknown = np.zeros_like(unknown_mask)
-    """
-    Take OR opertation of neighbour grid with mask of unknown cells, shifting
-    the mask of unknown cells to determine if a given cell has an neighbouring
-    cell that is marked as UNKNOWN
-    """ 
-    neighbour_unknown[1:, :] |= unknown_mask[:-1, :]
-    neighbour_unknown[:-1, :] |= unknown_mask[1:, :]
-    neighbour_unknown[:, 1:] |= unknown_mask[:, :-1]
-    neighbour_unknown[:, :-1] |= unknown_mask[:, 1:]
-    # AND mask of free cells and cells with UNKNOWN neighbours
-    return free_mask & neighbour_unknown
-
-"""
-Cluster cells of frontiers
-"""
-def cluster_frontiers(frontier_mask, min_size):
-    structure = np.ones((3,3), dtype=int) # 8-connectivity
-    clusters = []
-    labeled, num = ndimage.label(frontier_mask, structure=structure)
-
-    for label_id in range(1, num + 1):
-        ys, xs = np.where(labeled == label_id)
-        size = len(xs)
-        if (size < min_size):
-            continue
-        centroid_px = (float(np.mean(xs)), float(np.mean(ys)))
-        clusters.append({"label": label_id, "size":size, "centroid_px":centroid_px})
-    return clusters
-
-"""
-Convert grid cell indexs to world coords
-"""
-def grid_to_world(gx, gy, resolution, origin_x, origin_y):
-    world_x = origin_x + (gx + 0.5) * resolution
-    world_y = origin_y + (gy + 0.5) * resolution
-    return world_x, world_y
-
-"""
-Score + rank by combined utility
-"""
-def score_frontiers(clusters, resolution, origin_x, origin_y, robot_pose):
-    rx, ry = robot_pose
-    for c in clusters:
-        px, py = c["centroid_px"]
-        wx, wy = grid_to_world(px, py, resolution, origin_x, origin_y)
-        c["centroid"] = (wx, wy)
-        c["distance"] = float(np.hypot(wx - rx, wy - ry))
-        c["utility"] = self.w_info * c["size"] - self.w_cost * c["distance"]
-    
-    return sorted(clusters, key=lambda c: c["utility"], reverse=True)
-
 
 class FrontierSearch(Node):
     def __init__(self):
@@ -160,9 +100,9 @@ class FrontierSearch(Node):
 
         # Create publishers
         # ___________________________________________________________
-        self.goal_pub = self.create_publisher(
+        self.frontier_pub = self.create_publisher(
             PoseStamped,
-            'goal_pose',
+            'frontiers',
             10
         )
        
@@ -221,28 +161,126 @@ class FrontierSearch(Node):
 
     def odom_callback(self, msg: Odometry):
         self.latest_odom = msg
-
-    def get_robot_pose(self):
-        if self.latest_odom is None:
-            self.get_logger().warn("No odometry data received")
-            return None
-
-        p = self.latest_odom.pose.pose.position
-        return p.x, p.y
     
     # Use callback to get any new messages for OccupancyGrid
     def map_callback(self, msg:OccupancyGrid):
         self.latest_map = msg
 
+    # Node methods
+    # _______________________________________________________________
+    """
+    Find cells which form a frontier (FREE cells bordering UNKNOWN cells)
+    """
+    def find_frontier_mask(grid):
+        free_mask = grid == FREE
+        unknown_mask = grid == UNKNOWN
+
+        # Copy contents of unknown_mask, with same data type and shape
+        neighbour_unknown = np.zeros_like(unknown_mask)
+        """
+        Take OR opertation of neighbour grid with mask of unknown cells, shifting
+        the mask of unknown cells to determine if a given cell has an neighbouring
+        cell that is marked as UNKNOWN
+        """ 
+        neighbour_unknown[1:, :] |= unknown_mask[:-1, :]
+        neighbour_unknown[:-1, :] |= unknown_mask[1:, :]
+        neighbour_unknown[:, 1:] |= unknown_mask[:, :-1]
+        neighbour_unknown[:, :-1] |= unknown_mask[:, 1:]
+        # AND mask of free cells and cells with UNKNOWN neighbours
+        return free_mask & neighbour_unknown
+
+    """
+    Cluster cells of frontiers
+    """
+    def cluster_frontiers(frontier_mask):
+        structure = np.ones((3,3), dtype=int) # 8-connectivity
+        clusters = []
+        labeled, num = ndimage.label(frontier_mask, structure=structure)
+
+        for label_id in range(1, num + 1):
+            ys, xs = np.where(labeled == label_id)
+            size = len(xs)
+            centroid_px = (float(np.mean(xs)), float(np.mean(ys)))
+            # Check frontier against criterion
+            if (size < MIN_FRONTIER_SIZE or centroid_px < MIN_FRONTIER_DIST):
+                continue
+
+            clusters.append({"label": label_id, "size":size, "centroid_px":centroid_px})
+        return clusters
+
+    """
+    Convert grid cell indexs to world coords
+    """
+    def grid_to_world(gx, gy, resolution, origin_x, origin_y):
+        world_x = origin_x + (gx + 0.5) * resolution
+        world_y = origin_y + (gy + 0.5) * resolution
+        return world_x, world_y
+
+    """
+    Score + rank by combined utility
+    """
+    def score_frontiers(clusters, resolution, origin_x, origin_y, robot_pose):
+        rx, ry = robot_pose
+        for c in clusters:
+            px, py = c["centroid_px"]
+            wx, wy = grid_to_world(px, py, resolution, origin_x, origin_y)
+            c["centroid"] = (wx, wy)
+            c["distance"] = float(np.hypot(wx - rx, wy - ry))
+            c["utility"] = self.w_info * c["size"] - self.w_cost * c["distance"]
+        
+        return sorted(clusters, key=lambda c: c["utility"], reverse=True)
+
+    """
+    Get the current odometry data, provided by the callback function
+    """
+    def get_robot_pose(self):
+            if self.latest_odom is None:
+                self.get_logger().warn("No odometry data received")
+                return None
+
+            p = self.latest_odom.pose.pose.position
+            return p.x, p.y
+
+    """
+    Pacakge frontiers for publishing
+    """
+    def package_frontiers(self, clusters, resolution, origin):
+        
+        for c in clusters:
+            rows = [
+                (c["label"], c["size"],
+                origin.x + (c["centroid_px"][0] + 0.5) * resolution,
+                origin.y + (c["centroid_py"][1] + 0.5) * resolution)
+            ]          
+        packaged = np.array(rows, dtype=np.float32).reshape(-1, 4)
+        return packaged
+
+    """
+    Publish the packaged frontiers data
+    """
+    def publish_package(self, package):
+        msg = FrontierArray()
+        msg.header.stamp = self.get_clock().now.to_msg()
+        msg.header.frame_id = "map"
+        msg.rows, msg.cols = package.shape
+        msg.data = package.ravel().tolist()
+        self.frontier_pub.publish(msg)
+
+    """
+    Process all grid data and publish
+    """
     def process_grid(self):
+        # 1) Update map information
         if self.latest_map is None:
             self.get_logger().debug("No new map recieved")
             return
         
+        # 2) Update robot pose information
         robot_pose = self.get_robot_pose()
         if robot_pose is None:
             return
         
+        # 3) Break down occupancy grid message
         # Get msg from latest_map variable
         msg = self.latest_map
         # Process info from message
@@ -254,9 +292,11 @@ class FrontierSearch(Node):
 
         # Process message data from 1D array to 2D array (grid)
         grid = np.array(msg.data, dtype=np.int8).reshape((height, width))
-        # Determine mask grid for frontiers
+
+        # 4) Determine mask grid for frontiers
         frontier_mask = find_frontier_mask(grid)
-        # Determine clusters of frontiers using mask
+        
+        # 5) Determine clusters of frontiers using mask
         clusters = cluster_frontiers(frontier_mask, min_size=MIN_FRONTIER_SIZE)
 
         # Check if clusters were found
@@ -265,15 +305,14 @@ class FrontierSearch(Node):
             return
         self.get_logger().info(f"{len(clusters)} frontiers(s) found")
 
-        ranks = score_frontiers(clusters, resolution, origin_x, origin_y, robot_pose)
+        # -) Rank clusters
+        #ranked = score_frontiers(clusters, resolution, origin_x, origin_y, robot_pose)
 
-        best_frontier = ranks[0]
-        goal = PoseStamped()
-        goal.header.frame_id = 'map'
-        goal.header.stamp = self.get_clock().now().to_msg()
-        goal.pose.position.x, goal.pose.position.y = best_frontier["centroid"]
-        goal.pose.position.z = 0.0
-        goal.pose.orientation.w = 1.0
+        # 6) Package frontier clusters
+        package = package_frontiers(clusters, resolution, origin)
+        
+        # 7) Publish frontier data
+        publish_package(package)
 
         self.get_logger().info(
             f"goal=({goal.pose.position.x:.2f},{goal.pose.position.y:.2f}) "
